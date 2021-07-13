df8c73415a98574d9ae37c01f5fd0e4b5c63adb5
= <=>
2021-07-13 11:50:41 +0700

    Bai 9 bai tap 4

---
+++ bt4/bt4.js
@@ -0,0 +1,9 @@
+function anh(value){
+    let i = 1;
+    if (value==1){
+
+            if (i==1){document.getElementById("anh1").src = "img/anhmeo21.png"}
+            else if(i==2) {document.getElementById("anh1").src = "img/anhmeo22.png"}
+
+    }
+}
