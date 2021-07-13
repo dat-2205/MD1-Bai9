e3f65fa2fb535cde47b97702f6a09fdf4baf1b9f
= <=>
2021-07-13 14:27:25 +0700

    Bai 9 bai tap 4

--- bt4/bt4.js
+++ bt4/bt4.js
@@ -1,9 +1,61 @@
 function anh(value){
-    let i = 1;
+    check()
     if (value==1){
+            if (document.getElementById('anh1').src=='http://localhost:63342/MD1-Bai9/bt4/img/anhmeo11.png'){
+                document.getElementById("anh1").src = "img/anhmeo21.png"
+            } else if(document.getElementById('anh1').src=="http://localhost:63342/MD1-Bai9/bt4/img/anhmeo21.png") {
+                document.getElementById("anh1").src = "img/anhmeo31.png"
+            }else if(document.getElementById('anh1').src=="http://localhost:63342/MD1-Bai9/bt4/img/anhmeo31.png") {
+                document.getElementById("anh1").src = "img/anhmeo11.png"
+            }
+        check()
+    } else if (value==2){
+        if (document.getElementById('anh2').src=='http://localhost:63342/MD1-Bai9/bt4/img/anhmeo12.png'){
+            document.getElementById("anh2").src = "img/anhmeo22.png"
+        } else if(document.getElementById('anh2').src=="http://localhost:63342/MD1-Bai9/bt4/img/anhmeo22.png") {
+            document.getElementById("anh2").src = "img/anhmeo32.png"
+        }else if(document.getElementById('anh2').src=="http://localhost:63342/MD1-Bai9/bt4/img/anhmeo32.png") {
+            document.getElementById("anh2").src = "img/anhmeo12.png"
+        }
+        check()
 
-            if (i==1){document.getElementById("anh1").src = "img/anhmeo21.png"}
-            else if(i==2) {document.getElementById("anh1").src = "img/anhmeo22.png"}
+    } else if (value==3){
+        if (document.getElementById('anh3').src=='http://localhost:63342/MD1-Bai9/bt4/img/anhmeo13.png'){
+            document.getElementById("anh3").src = "img/anhmeo23.png"
+        } else if(document.getElementById('anh3').src=="http://localhost:63342/MD1-Bai9/bt4/img/anhmeo23.png") {
+            document.getElementById("anh3").src = "img/anhmeo33.png"
+        }else if(document.getElementById('anh3').src=="http://localhost:63342/MD1-Bai9/bt4/img/anhmeo33.png") {
+            document.getElementById("anh3").src = "img/anhmeo13.png"
+        }
+        check()
+    }
 
+}
+function check() {
+    let check = false;
+    if (document.getElementById("anh1").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo21.png'){
+        if(document.getElementById("anh2").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo22.png'){
+            if(document.getElementById("anh3").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo23.png'){
+               document.getElementById("container").style.background = "red";
+               check =true;
+            }
+        }
+    } else if (document.getElementById("anh1").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo31.png'){
+        if(document.getElementById("anh2").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo32.png'){
+            if(document.getElementById("anh3").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo33.png'){
+                document.getElementById("container").style.background = "blue";
+                check =true;
+            }
+        }
+    } else if (document.getElementById("anh1").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo11.png'){
+        if(document.getElementById("anh2").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo12.png'){
+            if(document.getElementById("anh3").src == 'http://localhost:63342/MD1-Bai9/bt4/img/anhmeo13.png'){
+                document.getElementById("container").style.background = "orange";
+                check =true;
+            }
+        }
+    }
+    if (!check){
+        document.getElementById("container").style.background = "white";
     }
 }
